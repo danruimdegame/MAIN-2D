909534c86f86332fa726d62a269cc75437a3828d
SrRuimdegame <danieltomaz1931@gmail.com>
2018-06-11 11:15:34 -0300

    Script MENU

---
+++ Assets/SCRIPT/MM_UIControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MM_UIControl : MonoBehaviour {
+
+    public void LoadScene(int indexNumber)
+    {
+        SceneManager.LoadScene(indexNumber);
+    }
+}

93a6dada3ce4d38009230873605a9504015fbe9e
SrRuimdegame <danieltomaz1931@gmail.com>
2018-06-18 12:10:24 -0300

    Criação Script Movimentação e Pulo

--- Assets/SCRIPT/Player.cs
+++ Assets/SCRIPT/Player.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+	public float horizontalSpeed = 10f;
+	public float jumpSpeed = 180f;
+
+	Rigidbody2D rb;
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		float horizontalInput = Input.GetAxisRaw("Horizontal");
+		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
+		if (horizontalPlayerSpeed != 0){
+			MoveHorizontal(horizontalPlayerSpeed);
+		}
+		else {
+			StopMoving();
+		}
+
+		if (Input.GetButtonDown("Jump")){
+			Jump();
+		}
+
+	}
+
+	void MoveHorizontal(float speed){
+		rb.velocity = new Vector2(speed, rb.velocity.y);
+	}
+
+	void StopMoving(){
+		rb.velocity = new Vector2(0f, rb.velocity.y);
+	}
+
+	void Jump(){
+		rb.velocity = new Vector2(0, jumpSpeed);
 	}
 }
